c134f042903c0f548c3b754576b9743eb7f3cc9f
Peter Kim <peterkim23@gmail.com>
2016-12-18 16:07:37 -0800

    completed Node Beginner Book code sample.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var server = require('./server.js');
 var router = require('./router.js');
+var requestHandlers = require('./requestHandlers.js');
 
+var handle = {};
 
-server.start(router.route);
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -3,16 +3,11 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log('Request for ' + pathname + 'received.');
-
-    route(pathname);
-
-    response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+    console.log('Request for ' + pathname + ' received.');
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
